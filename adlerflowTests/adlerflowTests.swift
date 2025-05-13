e93dea415fc8c28e41a150c194fce65696258810
Constantin Adler <constantin.adler@icloud.com>
2025-05-13 09:25:37 +0200

    Initial Commit

---
+++ adlerflowTests/adlerflowTests.swift
@@ -0,0 +1,16 @@
+//
+//  adlerflowTests.swift
+//  adlerflowTests
+//
+//  Created by adler on 13.05.2025.
+//
+
+import Testing
+
+struct adlerflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
